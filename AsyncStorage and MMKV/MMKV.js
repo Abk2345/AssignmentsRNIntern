bbd764d4fb0faa6732f10bdd735aae4c74f6e709
Abk2345 <abhishantkumar138@gmail.com>
2023-08-28 22:41:34 +0530

    Assignment1: Asyncstorage + MMKV updated

--- AsyncStorage and MMKV/MMKV.js
+++ AsyncStorage and MMKV/MMKV.js
@@ -1,19 +1,33 @@
-// react-native module needs to run on native react environment locally installed after ejecting from expo, works great
 import { MMKVLoader, useMMKVStorage } from 'react-native-mmkv-storage';
-import {Text, View} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const storage = new MMKVLoader().initialize();
 const App = () => {
   const [user, setUser] = useMMKVStorage('user', storage, 'robert');
   const [age, setAge] = useMMKVStorage('age', storage, 24);
 
+  const changeContent = ()=>{
+    setUser('Abhishant');
+    setAge(22);
+  }
+
   return (
     <View style={styles.header}>
       <Text style={styles.headerText}>
         I am {user} and I am {age} years old.
       </Text>
+      <Button title= "change content" onPress={changeContent}></Button>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  header:{
+
+  },
+  headerText: {
+
+  }
+})
