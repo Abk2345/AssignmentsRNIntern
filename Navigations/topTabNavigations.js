21df99dc21961ffb3e4f2e7c76760a494633648a
Abk2345 <abhishantkumar138@gmail.com>
2023-08-29 20:16:28 +0530

    Assignment #15: Expo + React Navigation 6.0

---
+++ Navigations/topTabNavigations.js
@@ -0,0 +1,36 @@
+// material top tabs navigation
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{fontSize: 20, fontWeight: 'bold'}}>Settings!</Text>
+    </View>
+  );
+}
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
